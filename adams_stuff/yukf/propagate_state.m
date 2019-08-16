89d99da143be4b82b3b5aa0b4847b543cf1bb93a
Adam Caccavale <adam.w.caccavale@gmail.com>
2019-08-15 18:12:32 -0700

    Realized the way the state was being propagated did not preserve the quaternions constraints. Changed this but I am still having issues when the innovation is added into the state vector in the final part of the ukf

--- adams_stuff/yukf/propagate_state.m
+++ adams_stuff/yukf/propagate_state.m
@@ -13,20 +13,38 @@ function est_next_state = propagate_state(state, model, u)
     end
 
     % Predict Quaternions
-    % omegas
-    wx = state(10);
-    wy = state(11);
-    wz = state(12);
-
-    % Setup Some Useful Stuff for Pred 
-    Omega = [ 0 -wx -wy -wz ;...
-             wx   0  wz -wy ;...
-             wy -wz   0  wx ;...
-             wz  wy -wx   0 ];
-
     quat = complete_unit_quat(state(7:9));
-    q_hat = quat + 0.5 * Omega * quat * model.con_dt;
+    
+    b_use_paper_method = true;
+    if b_use_paper_method
+        w_vec = state(10:12);
+        % convert omega to angle axis
+%         nrm = norm(w_vec);
+%         ang = nrm * model.con_dt;
+%         ax =  w_vec / nrm;
+%         quat_delta = axang_to_quat(ax*ang);
+        quat_delta = axang_to_quat(w_vec);
+        q_hat = quatmultiply(quat(:)', quat_delta(:)')';
+    else
+        % omegas
+        wx = state(10);
+        wy = state(11);
+        wz = state(12);
+
+        % Setup Some Useful Stuff for Pred 
+        Omega = [ 0 -wx -wy -wz ;...
+                 wx   0  wz -wy ;...
+                 wy -wz   0  wx ;...
+                 wz  wy -wx   0 ];
+
+        q_hat = quat + 0.5 * Omega * quat * model.con_dt;
+    end
     est_next_state(7:9) = q_hat(2:4);
+    vec_norm = norm( est_next_state(7:9) );
+    if( vec_norm > 1 && vec_norm < 1.001 )
+        % just a numerical thing
+        est_next_state(7:9) = est_next_state(7:9) / vec_norm;
+    end
 
     % Predict Angular Velocities
     if(~isempty(u))
