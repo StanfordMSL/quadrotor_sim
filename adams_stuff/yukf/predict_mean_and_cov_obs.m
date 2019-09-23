0580b46f16eb5b6ced3a578b9985b09d4271d164
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-09-23 14:12:31 -0700

    Fix obs covariance sum

--- adams_stuff/yukf/predict_mean_and_cov_obs.m
+++ adams_stuff/yukf/predict_mean_and_cov_obs.m
@@ -1,13 +1,14 @@
 function [z_hat, S, S_inv] = predict_mean_and_cov_obs(pred_obs, yukf, cov_add_noise)
     % inverse of the unscented transform
-    dim = size(pred_obs, 1);
+    dim = length(yukf.mu);
+    %size(pred_obs, 2);
     
     % Calculate mean
     z_hat = (yukf.w0_m * pred_obs(:, 1)) + (yukf.wi * sum(pred_obs(:, 2:end), 2));    
     
     % Calculate covar
     S = yukf.w0_c * (pred_obs(:, 1 )- z_hat) * (pred_obs(:, 1) - z_hat)';
-    for obs_ind = 2:dim
+    for obs_ind = 1:dim
         S = S + yukf.wi * (pred_obs(:, obs_ind + 1) - z_hat) * (pred_obs(:, obs_ind + 1) - z_hat)';
         S = S + yukf.wi * (pred_obs(:, dim + obs_ind + 1) - z_hat) * (pred_obs(:, dim + obs_ind + 1) - z_hat)';
     end
