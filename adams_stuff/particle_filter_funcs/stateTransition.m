16c06f9a1ad1c62e2ae138f3be4a8cc4853a3e5b
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-09-26 11:36:22 -0700

    Fix rotation in noise for propagating particles

--- adams_stuff/particle_filter_funcs/stateTransition.m
+++ adams_stuff/particle_filter_funcs/stateTransition.m
@@ -4,7 +4,14 @@ function predictParticles = stateTransition(pf,prevParticles,model,u,dt,process_
     predictParticles = zeros(pf.NumParticles,pf.NumStateVariables);
     for part_ind = 1:pf.NumParticles
         predictParticles(part_ind,:) = propagate_state(prevParticles(part_ind,:), model, u, dt);
-        predictParticles(part_ind,:) = normrnd(predictParticles(part_ind,:)',diag(process_noise));
+        predictParticles_ax_ang = predictParticles(part_ind,:)';
+        quat = complete_unit_quat(predictParticles_ax_ang(7:9));
+        predictParticles_ax_ang(7:9) = quat_to_axang(quat) ;
+        predictParticles_ax_ang = normrnd(predictParticles_ax_ang,diag(process_noise));
+        predictParticles(part_ind,:) = predictParticles_ax_ang;
+        quat = axang_to_quat(predictParticles_ax_ang(7:9));
+        predictParticles(part_ind,7:9) = quat(2:4);
+        
     end
     % Keep the best particle
     predictParticles(1,:) = propagate_state(pf.State, model, u, dt);
