8146d94312f6b1eb2769264018c70119641e366e
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-10-24 23:08:27 -0700

    Set up everything to learn to track pitch from bounding box

---
+++ learn_pitch_mlp/data_loader/data_loaders.py
@@ -0,0 +1,107 @@
+from torchvision import datasets, transforms
+from base import BaseDataLoader
+from torch.utils.data import Dataset
+import numpy as np
+from pyquaternion import Quaternion
+import torch
+import os
+
+def load_bounding_boxes(bb_path):
+    bb_file = open(bb_path, 'r')
+    lines = bb_file.readlines()
+    bounding_boxes = [line.split()[2:] for line in lines]
+    bounding_boxes = [[float(i) for i in line] for line in bounding_boxes]
+    return bounding_boxes
+
+
+def load_pitches(poses_path, file_name_prefix):
+    pose_files = os.listdir(poses_path)
+    # pose_files.sort()
+    ids = [int(pose_file.split('.')[0][len(file_name_prefix):])
+           for pose_file in pose_files]
+    ids.sort()
+    pitches = []
+    for id in ids:
+        filepath = poses_path+file_name_prefix+str(id)+'.txt'
+        pose_file = open(filepath, 'r')
+        q = Quaternion(pose_file.readlines()[0].split()[3:7])
+        (y, p, r) = q.yaw_pitch_roll
+        pitches.append(p)
+    return pitches  # inputs
+
+
+def preprocess_inputs(boxes):
+    # Bring between 0 and 1
+    img_height = 480
+    img_width = 640
+    np_boxes = np.array(boxes)
+    x_pos = [0, 2, 4, 6]
+    y_pos = [1, 3, 5, 7]
+    np_boxes[:, x_pos] = np_boxes[:, x_pos]/img_width
+    np_boxes[:, y_pos] = np_boxes[:, y_pos]/img_height
+    return list(np_boxes)
+
+# From https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+class BoundingBoxPitchDataset(Dataset):
+    """Bounding Box - Pitch dataset."""
+
+    def __init__(self, bounding_boxes_file, pose_files_folder, poses_files_prefix, sequence_length, img_height=480, img_width = 640):
+
+        # Load boxes and pitch
+        self.bounding_boxes = load_bounding_boxes(bounding_boxes_file)
+        self.bounding_boxes = preprocess_inputs(self.bounding_boxes)
+        self.pitches = load_pitches(pose_files_folder, poses_files_prefix)
+
+        # Pad with 0s at the beginning
+        nb_to_add = sequence_length - len(self.bounding_boxes)%sequence_length
+
+        for i in np.arange(nb_to_add):
+            self.bounding_boxes.insert(0,np.array([0.,0.,0.,0.,0.,0.,0.,0.]))
+            self.pitches.insert(0,0.0)
+        # Gather into blocks of sequences
+        self.bounding_boxes = torch.FloatTensor(list(
+            chunks(self.bounding_boxes, sequence_length))).reshape(-1,8*sequence_length)
+        self.pitches = torch.FloatTensor(list(
+            chunks(self.pitches, sequence_length)))
+
+    def __len__(self):
+        return len(self.bounding_boxes)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        # sample = {'bounding_box': np.array(self.bounding_boxes[idx]), 'pitch': np.array(self.pitches[idx])}
+        # return sample
+        return self.bounding_boxes[idx], self.pitches[idx][-1]
+
+
+class MnistDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        print(self.dataset)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class BoundingBoxPitchDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+
+    def __init__(self, bounding_boxes_file, pose_files_folder, poses_files_prefix, batch_size, sequence_length, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self.dataset = BoundingBoxPitchDataset(
+            bounding_boxes_file, pose_files_folder, poses_files_prefix, sequence_length)
+        print(self.dataset)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
---
+++ learn_pitch_mlp/model/model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from base import BaseModel
+
+
+class MnistModel(BaseModel):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+class PitchModel(BaseModel):
+    def __init__(self, sequence_length=20):
+        super().__init__()
+        self.fc1 = nn.Linear(8*sequence_length, 100)
+        self.fc2 = nn.Linear(100, 1)
+
+    def forward(self, x):
+        x = self.fc2(self.fc1(x))
+        return x
