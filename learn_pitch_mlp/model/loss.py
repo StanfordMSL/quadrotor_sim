0232a46e7b2642a2db13f891991f4bc61a19c8fe
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-10-28 13:35:54 -0700

    Add lmore loss functions

--- learn_pitch_mlp/model/loss.py
+++ learn_pitch_mlp/model/loss.py
@@ -5,3 +5,24 @@ def mse_loss_deg(output,target):
     output = output*180/math.pi
     target = target*180/math.pi
     return F.mse_loss(output,target)
+
+def mse_loss(output,target):
+    return F.mse_loss(output,target)
+
+def l1_loss(output,target):
+    return F.l1_loss(output, target)
+
+
+def l1_loss_deg(output, target):
+    output = output*180/math.pi
+    target = target*180/math.pi
+    return F.l1_loss(output, target)
+
+def smooth_l1_loss(output, target):
+    return F.smooth_l1_loss(output, target)
+
+
+def smooth_l1_loss_deg(output, target):
+    output = output*180/math.pi
+    target = target*180/math.pi
+    return F.smooth_l1_loss(output, target)
