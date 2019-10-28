98b887f68546772e8a5ab66135c9552207f67923
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-10-28 13:22:23 -0700

    Add possibility to downsample and filter low pitches when loading the data

--- learn_pitch_mlp/data_loader/data_loaders.py
+++ learn_pitch_mlp/data_loader/data_loaders.py
@@ -6,12 +6,26 @@ from pyquaternion import Quaternion
 import torch
 import os
 
+
+def filter_data(bounding_boxes, pitches, pitch_thresh):
+    ret_pitches = pitches.copy()
+    nb_removed = 0
+    for i, pitch in enumerate(pitches):
+        if np.abs(pitch[-1]) < pitch_thresh:
+            bounding_boxes.pop(i-nb_removed)
+            ret_pitches.pop(i-nb_removed)
+            nb_removed += 1
+    return bounding_boxes,ret_pitches
+
+
+
 def get_ids_in_path(path, prefix):
     files = os.listdir(path)
     ids = [int(file.split('.')[0][len(prefix):])
            for file in files]
     ids.sort()
     return ids
+
 
 def load_bounding_boxes(bb_file_path):
     bb_file = open(bb_file_path, 'r')
@@ -31,7 +45,7 @@ def load_pitches(pose_file_path):
     pitches = []
     for pose in poses:
         q = Quaternion(pose[3:7])
-        (y,p,r) = q.yaw_pitch_roll
+        (y, p, r) = q.yaw_pitch_roll
         pitches.append(p)
     return pitches
 
@@ -48,47 +62,62 @@ def preprocess_inputs(boxes):
     return list(np_boxes)
 
 # From https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+
+
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l)-n):
         yield l[i:i + n]
 
+
 class BoundingBoxPitchDataset(Dataset):
     """Bounding Box - Pitch dataset."""
 
-    def __init__(self, bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, sequence_length, img_height=480, img_width = 640):
+    def __init__(self, bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, sequence_length, downsample_rate=1, filter_pitch = False, pitch_threshold=3.14159, img_height=480, img_width=640):
 
         # Load boxes and pitch
-        bb_ids = get_ids_in_path(bounding_boxes_path,bbs_files_prefix)
-        pose_ids = get_ids_in_path(pose_files_path,poses_files_prefix)
-        assert bb_ids == pose_ids, print("Ids of pose and bounding boxes don't match")
+        bb_ids = get_ids_in_path(bounding_boxes_path, bbs_files_prefix)
+        pose_ids = get_ids_in_path(pose_files_path, poses_files_prefix)
+        assert bb_ids == pose_ids, print(
+            "Ids of pose and bounding boxes don't match")
 
         self.bounding_boxes = []
         self.pitches = []
 
         for id in bb_ids:
+            # Downsample
+            if id % downsample_rate != 0:
+                continue
             bb_file = bounding_boxes_path+bbs_files_prefix+str(id)+'.txt'
             poses_file = pose_files_path+poses_files_prefix+str(id)+'.txt'
             bounding_boxes = load_bounding_boxes(bb_file)
             bounding_boxes = preprocess_inputs(bounding_boxes)
             pitches = load_pitches(poses_file)
 
-            assert len(bounding_boxes) == len(pitches), print("Not as many bounding boxes as poses for id "+str(id))
+            assert len(bounding_boxes) == len(pitches), print(
+                "Not as many bounding boxes as poses for id "+str(id))
 
             # Pad with 0s at the beginning
-            nb_to_add = sequence_length - len(bounding_boxes)%sequence_length
+            nb_to_add = sequence_length - len(bounding_boxes) % sequence_length
 
             for i in np.arange(nb_to_add):
-                bounding_boxes.insert(0,np.array([0.,0.,0.,0.,0.,0.,0.,0.]))
-                pitches.insert(0,0.0)
+                bounding_boxes.insert(0, np.array(
+                    [0., 0., 0., 0., 0., 0., 0., 0.]))
+                pitches.insert(0, 0.0)
 
             # Gather into blocks of sequences
-            self.bounding_boxes.extend(list(chunks(bounding_boxes, sequence_length)))
+            self.bounding_boxes.extend(
+                list(chunks(bounding_boxes, sequence_length)))
             self.pitches.extend(list(chunks(pitches, sequence_length)))
 
-        self.bounding_boxes = torch.FloatTensor(self.bounding_boxes).reshape(-1,8*sequence_length)
-        self.pitches = torch.FloatTensor(self.pitches).reshape(-1,sequence_length)
+        if filter_pitch:
+            (self.bounding_boxes, self.pitches) = filter_data(
+                self.bounding_boxes, self.pitches, pitch_threshold)
 
+        self.bounding_boxes = torch.FloatTensor(
+            self.bounding_boxes).reshape(-1, 8*sequence_length)
+        self.pitches = torch.FloatTensor(
+            self.pitches).reshape(-1, sequence_length)
 
     def __len__(self):
         return len(self.bounding_boxes)
@@ -105,13 +134,15 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
+
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = datasets.MNIST(
+            self.data_dir, train=training, download=True, transform=trsfm)
         print(self.dataset)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
@@ -121,8 +152,8 @@ class BoundingBoxPitchDataLoader(BaseDataLoader):
     MNIST data loading demo using BaseDataLoader
     """
 
-    def __init__(self, bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, batch_size, sequence_length, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, batch_size, sequence_length, downsample_rate, filter_pitch, pitch_threshold, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         self.dataset = BoundingBoxPitchDataset(
-            bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, sequence_length)
+            bounding_boxes_path, pose_files_path, poses_files_prefix, bbs_files_prefix, sequence_length, downsample_rate, filter_pitch, pitch_threshold)
         print(self.dataset)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
