Fix quaternion read on real data

learn_pitch_mlp/model/metric.py
@@ -2,8 +2,10 @@ import torch
 import math
 
 def mse_deg(output,target):
-    output = output*180/math.pi
-    target = target*180/math.pi
+    output = output[:,-1]*180/math.pi
+    target = target[:,-1]*180/math.pi
+    # for i in range(len(output)):
+    #     print(str(output[i]/180*math.pi)+' '+str(target[i]*math.pi/180))
     return torch.mean((output-target)*(output-target))
 
 def std_deg(output,target):