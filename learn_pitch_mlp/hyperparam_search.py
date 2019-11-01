c1be03b69fa09cdf77e1b2fc7403a69c021cfeed
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-11-01 08:45:48 -0700

    Add batch size, optimizer, and learning rate in hyper param search

--- learn_pitch_mlp/hyperparam_search.py
+++ learn_pitch_mlp/hyperparam_search.py
@@ -13,7 +13,7 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 with open('hyperparam_search_results.txt', 'w') as f:
-    f.write("# seq_len hidden_units levels kernel_size mse_deg std_deg val_mse_deg val_std_deg\n")
+    f.write("# seq_len hidden_units levels kernel_size lr batch_size optimizer mse_deg std_deg val_mse_deg val_std_deg\n")
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
@@ -38,20 +38,36 @@ if __name__ == '__main__':
     hidden_units = [5, 15, 30,100]
     levels = [1, 3,5,7,10]
     kernel_size = [3,5,7,10]
-    for seq_len_i in seq_len:
-        for hidden_units_i in hidden_units:
-            for levels_i in levels:
-                for kernel_size_i in kernel_size:
-                    config['arch']['args']['sequence_length'] = seq_len_i
-                    config['arch']['args']['hidden_units_per_layer'] = hidden_units_i
-                    config['arch']['args']['levels'] = levels_i
-                    config['arch']['args']['kernel_size'] = kernel_size_i
+    lr = [0.1,0.01,0.001,0.0001,0.00001]
+    batch_size = [128,64,32,16,8]
+    optimizer = [0,1] # 0 for Adam, 1 for SGD
+    for optimizer_i in optimzer:
+        for lr_i in lr:
+            for batch_size_i in batch_size:
+                for seq_len_i in seq_len:
+                    for hidden_units_i in hidden_units:
+                        for levels_i in levels:
+                            for kernel_size_i in kernel_size:
+                                config['arch']['args']['sequence_length'] = seq_len_i
+                                config['arch']['args']['hidden_units_per_layer'] = hidden_units_i
+                                config['arch']['args']['levels'] = levels_i
+                                config['arch']['args']['kernel_size'] = kernel_size_i
+                                config['data_loader']['args']['batch_size'] = batch_size_i
+                                config['optimizer']['args']['lr'] = lr_i
+                                if optimizer_i == 0:
+                                    config['optimizer']['args']['type'] = 'Adam'
+                                elif optimizer_i == 1:
+                                    config['optimizer']['args']['type'] = 'SGD'
+                                else:
+                                    print('Unknown optimizer')
+                                    exit
 
-                    config['data_loader']['args']['sequence_length'] = seq_len_i
-                    config['trainer']['save_dir'] = 'saved_seq'+str(seq_len_i)+'_h'+str(
-                        hidden_units_i)+'_lev'+str(levels_i)+'_k'+str(kernel_size_i)+'/'
-                    res = main(config)
-                    print(res)
-                    with open('hyperparam_search_results.txt', 'a') as f:
-                        f.write(str(seq_len_i)+' '+str(hidden_units_i)+' '+str(levels_i)+' '+str(kernel_size_i)+' '+str(
-                            res['mse_deg'].item())+' '+str(res['std_deg'].item())+' '+str(res['val_mse_deg'].item())+' '+str(res['val_std_deg'].item())+'\n')
+                                config['optimizer']['args']['type'] = optimizer_i
+                                config['data_loader']['args']['sequence_length'] = seq_len_i
+                                # config['trainer']['save_dir'] = 'saved_seq'+str(seq_len_i)+'_h'+str(
+                                #     hidden_units_i)+'_lev'+str(levels_i)+'_k'+str(kernel_size_i)+'/'
+                                res = main(config)
+                                print(res)
+                                with open('hyperparam_search_results.txt', 'a') as f:
+                                    f.write(str(seq_len_i)+' '+str(hidden_units_i)+' '+str(levels_i)+' '+str(kernel_size_i)+' '+str(lr_i)+' '+str(batch_size_i)+' '+str(optimizer_i)+' '+str(
+                                        res['mse_deg'].item())+' '+str(res['std_deg'].item())+' '+str(res['val_mse_deg'].item())+' '+str(res['val_std_deg'].item())+'\n')
