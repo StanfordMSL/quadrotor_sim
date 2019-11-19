f5cb1032730142626c1d6db01a00e21969eb84ee
JE Low <lowjunen@gmail.com>
2019-11-18 16:19:39 -0800

    Move ros_ws to the DESI repo

--- ros_ws/build/sim_link/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lowjunen/StanfordMSL/quadrotor_sim/ros_ws/src/sim_link
-# Build directory: /home/lowjunen/StanfordMSL/quadrotor_sim/ros_ws/build/sim_link
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
